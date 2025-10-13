8f1aca159358d5c494d71da9d3a2d687d4117634
duyenqa <ttemp8399@gmail.com>
2025-10-13 17:12:22 +0700

    add animation share apps

--- src/components/share/ShareApps.tsx
+++ src/components/share/ShareApps.tsx
@@ -15,7 +15,7 @@ import {
 } from "react-share";
 import styles from "./shareapps.module.css";
 import { shareUrl } from "@/utils/constant";
-import { Fab, Tooltip } from "@mui/material";
+import { Collapse, Fab, Tooltip } from "@mui/material";
 import ShareIcon from '@mui/icons-material/Share';
 
 
@@ -24,39 +24,40 @@ const ShareApps = () => {
 
     return (
         <div className={styles.shareApps}>
-            {showApps == false ? null : <div className={styles.apps}>
-                <Tooltip title="Facebook" placement="left">
-                    <FacebookShareButton url={shareUrl}>
-                        <FacebookIcon size={40} round={true} />
-                    </FacebookShareButton>
-                </Tooltip>
-                <Tooltip title="Twitter" placement="left">
-                    <TwitterShareButton url={shareUrl}>
-                        <TwitterIcon size={40} round={true} />
-                    </TwitterShareButton>
-                </Tooltip>
-                <Tooltip title="Email" placement="left">
-                    <EmailShareButton url={shareUrl}>
-                        <EmailIcon size={40} round={true} />
-                    </EmailShareButton>
-                </Tooltip>
-                <Tooltip title="Viber" placement="left">
-                    <ViberShareButton url={shareUrl}>
-                        <ViberIcon size={40} round={true} />
-                    </ViberShareButton>
-                </Tooltip>
-                <Tooltip title="Line" placement="left">
-                    <LineShareButton url={shareUrl}>
-                        <LineIcon size={40} round={true} />
-                    </LineShareButton>
-                </Tooltip>
-                <Tooltip title="Whatsapp" placement="left">
-                    <WhatsappShareButton url={shareUrl}>
-                        <WhatsappIcon size={40} round={true} />
-                    </WhatsappShareButton>
-                </Tooltip>
-            </div>
-            }
+            <Collapse in={showApps}>
+                <div className={styles.apps}>
+                    <Tooltip title="Facebook" placement="left">
+                        <FacebookShareButton url={shareUrl}>
+                            <FacebookIcon size={40} round={true} />
+                        </FacebookShareButton>
+                    </Tooltip>
+                    <Tooltip title="Twitter" placement="left">
+                        <TwitterShareButton url={shareUrl}>
+                            <TwitterIcon size={40} round={true} />
+                        </TwitterShareButton>
+                    </Tooltip>
+                    <Tooltip title="Email" placement="left">
+                        <EmailShareButton url={shareUrl}>
+                            <EmailIcon size={40} round={true} />
+                        </EmailShareButton>
+                    </Tooltip>
+                    <Tooltip title="Viber" placement="left">
+                        <ViberShareButton url={shareUrl}>
+                            <ViberIcon size={40} round={true} />
+                        </ViberShareButton>
+                    </Tooltip>
+                    <Tooltip title="Line" placement="left">
+                        <LineShareButton url={shareUrl}>
+                            <LineIcon size={40} round={true} />
+                        </LineShareButton>
+                    </Tooltip>
+                    <Tooltip title="Whatsapp" placement="left">
+                        <WhatsappShareButton url={shareUrl}>
+                            <WhatsappIcon size={40} round={true} />
+                        </WhatsappShareButton>
+                    </Tooltip>
+                </div>
+            </Collapse>
             <Fab aria-label="like" color="warning" onClick={() => setShowApps(!showApps)}>
                 <ShareIcon />
             </Fab>
