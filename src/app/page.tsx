3c0a8f0698b443e920955edd4af4d4c1e4c904cb
duyenqa <ttemp8399@gmail.com>
2025-12-12 12:48:38 +0700

    clean trash

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,7 @@ import { useTheme } from "@/context/ThemeContext";
 import Input from "@/components/textfield/Input";
 import Footer from "@/components/footer/Footer";
 import ShareApps from "@/components/share/ShareApps";
-import { Alert, AlertTitle, Box, Button, Chip, Dialog, DialogActions, DialogContent, DialogContentText, Typography } from "@mui/material";
-import DialogTitle from '@mui/material/DialogTitle';
+import { Alert, AlertTitle, Box, Button, Chip, Typography } from "@mui/material";
 import SearchIcon from '@mui/icons-material/Search';
 import PhoneIcon from '@mui/icons-material/Phone';
 import MessageIcon from '@mui/icons-material/Message';
@@ -21,15 +20,14 @@ export default function Home() {
   const [notification, setNotification] = useState<IPhones[] | undefined>(undefined);
   const [showMessage, setShowMessage] = useState<string>("");
   const [textSearch, setTextSearch] = useState<string>(" ");
-  const [open, setOpen] = useState<boolean>(false);
   const [statusSearch, setStatusSearch] = useState<boolean>(false);
   const [phone, setPhone] = useState<string | null>(" ");
   const [selectedCard, setSelectedCard] = useState<number | null>(null);
 
   const fetchPhoneNumbers = async () => {
     const { data, error } = await supabase
-        .from('phones')
-        .select('*')
+      .from('phones')
+      .select('*')
 
     if (error) console.error('Lỗi:', error);
     else setNotification(data);
@@ -37,7 +35,7 @@ export default function Home() {
 
   useEffect(() => {
     fetchPhoneNumbers();
-  },[])
+  }, [])
 
   function onSearch(text: string) {
     if (!notification || notification.length === 0 || !text.trim()) {
@@ -68,156 +66,122 @@ export default function Home() {
   }
 
   return (
-    <>
-      <section className={`${styles.page} ${styles[theme]}`}>
-        <div className={styles.wrapper}>
-          <div className={styles.navbar}>
-            <Chip
-              label={theme === 'light' ? 'Sáng' : 'Tối'}
-              icon={theme === 'light' ? <WbSunnyOutlinedIcon /> : <Brightness2OutlinedIcon />}
-              color="primary"
-              onClick={toggleTheme}
-              variant="outlined"
-              size="medium"
-            />
-          </div>
-          <div className={styles.searchBar}>
-            <Input handleChangeText={onChangeTextSearch} filterData={onSearch} />
-            <Button
-              variant="contained"
-              size="medium"
-              onClick={() => onSearch(textSearch)}
-            >
-              <SearchIcon />
-            </Button>
-          </div>
-          <main className={styles.main}>
-            <div className={styles.recommendUser}>
-              <Box component="section" sx={{ my: '1rem' }}>
-                {showMessage && (
-                  <>
-                    <Typography variant="h5" gutterBottom sx={{ color: theme === 'light' ? '#000' : '#fff' }}>Số điện thoại {textSearch.length < 4 ? textSearch : phone}</Typography>
-                    <Typography variant="subtitle1" gutterBottom sx={{ color: theme === 'light' ? '#000' : '#fff' }}>{showMessage}</Typography>
-                  </>
-                )}
-                {statusSearch == true && showMessage != "Không tìm thấy dữ liệu!" && (
-                  <Alert severity="warning">
-                    <AlertTitle>Cảnh báo</AlertTitle>
-                    <li>1.Không gọi lại sau khi nhận cuộc gọi nhá máy từ số này.</li>
-                    <li>2.Cần chặn số điện thoại để ngăn chặn cuộc gọi làm phiền trong tương lai.</li>
-                    <li>3.Nếu thấy có dấu hiệu lừa đảo, nên báo cáo cho nhà mạng để có biện pháp can thiệp kịp thời.</li>
-                  </Alert>
-                )}
-              </Box>
-              <Box component="section" sx={{ p: 2, my: 5, backgroundColor: '#a4d1d6' }}>
-                <Typography variant="h5" gutterBottom sx={{ color: theme === 'light' ? '#000' : '#fff' }}>Cách nhận biết số rác / lừa đảo</Typography>
-                <div className={styles.list}>
-                  <div className={styles.scrollVertical}>
-                    <div className={styles.information}>
-                      {phonesSpam.map((spam) => (
-                        <div className={styles.contentThrief} key={spam.id}>
-                          <p>{spam.title}</p>
-                        </div>
-                      ))}
-                    </div>
-                  </div>
-                  <div className={styles.introduce}>
-                    <Typography
-                      variant="subtitle1"
-                      gutterBottom
-                      sx={{ color: theme === 'light' ? '#000' : '#fff' }}
-                      >
-                      {introduceText01}
-                    </Typography>
-                    <Typography
-                      variant="subtitle1"
-                      gutterBottom
-                      sx={{ color: theme === 'light' ? '#000' : '#fff' }}
-                      >
-                      {introduceText02}
-                    </Typography>
+    <section className={`${styles.page} ${styles[theme]}`}>
+      <div className={styles.wrapper}>
+        <div className={styles.navbar}>
+          <Chip
+            label={theme === 'light' ? 'Sáng' : 'Tối'}
+            icon={theme === 'light' ? <WbSunnyOutlinedIcon /> : <Brightness2OutlinedIcon />}
+            color="primary"
+            onClick={toggleTheme}
+            variant="outlined"
+            size="medium"
+          />
+        </div>
+        <div className={styles.searchBar}>
+          <Input handleChangeText={onChangeTextSearch} filterData={onSearch} />
+          <Button
+            variant="contained"
+            size="medium"
+            onClick={() => onSearch(textSearch)}
+          >
+            <SearchIcon />
+          </Button>
+        </div>
+        <main className={styles.main}>
+          <div className={styles.recommendUser}>
+            <Box component="section" sx={{ my: '1rem' }}>
+              {showMessage && (
+                <>
+                  <Typography variant="h5" gutterBottom sx={{ color: theme === 'light' ? '#000' : '#fff' }}>Số điện thoại {textSearch.length < 4 ? textSearch : phone}</Typography>
+                  <Typography variant="subtitle1" gutterBottom sx={{ color: theme === 'light' ? '#000' : '#fff' }}>{showMessage}</Typography>
+                </>
+              )}
+              {statusSearch == true && showMessage != "Không tìm thấy dữ liệu!" && (
+                <Alert severity="warning">
+                  <AlertTitle>Cảnh báo</AlertTitle>
+                  <li>1.Không gọi lại sau khi nhận cuộc gọi nhá máy từ số này.</li>
+                  <li>2.Cần chặn số điện thoại để ngăn chặn cuộc gọi làm phiền trong tương lai.</li>
+                  <li>3.Nếu thấy có dấu hiệu lừa đảo, nên báo cáo cho nhà mạng để có biện pháp can thiệp kịp thời.</li>
+                </Alert>
+              )}
+            </Box>
+            <Box component="section" sx={{ p: 2, my: 5, backgroundColor: '#a4d1d6' }}>
+              <Typography variant="h5" gutterBottom sx={{ color: theme === 'light' ? '#000' : '#fff' }}>Cách nhận biết số rác / lừa đảo</Typography>
+              <div className={styles.list}>
+                <div className={styles.scrollVertical}>
+                  <div className={styles.information}>
+                    {phonesSpam.map((spam) => (
+                      <div className={styles.contentThrief} key={spam.id}>
+                        <p>{spam.title}</p>
+                      </div>
+                    ))}
                   </div>
                 </div>
-              </Box>
-              <Box component="section" sx={{ p: 2, backgroundColor: '#a4d1d6' }}>
-                <div className={styles.introduceNumber}>
-                  <Typography 
-                    variant="h5" 
+                <div className={styles.introduce}>
+                  <Typography
+                    variant="subtitle1"
                     gutterBottom
                     sx={{ color: theme === 'light' ? '#000' : '#fff' }}
                   >
-                    Giới thiệu
+                    {introduceText01}
                   </Typography>
                   <Typography
                     variant="subtitle1"
                     gutterBottom
-                    sx={{ color: theme === 'light' ? '#000' : '#fff',
-                      width: '65%',
-                      textAlign: 'center',
-                      lineHeight: 2
-                     }}
+                    sx={{ color: theme === 'light' ? '#000' : '#fff' }}
                   >
-                    Tổng đài 156 là đầu số miễn phí của Bộ Thông tin và Truyền thông, dùng để tiếp nhận phản ánh về cuộc gọi rác, tin nhắn rác và các cuộc gọi có dấu hiệu lừa đảo. Áp dụng toàn quốc, trên tất cả các nhà mạng di động và cố định.
+                    {introduceText02}
                   </Typography>
                 </div>
-                <div className={styles.cards}>
-                  {cards.map((card, index) => (
-                    <div className={`${styles.card} ${selectedCard == index ? styles.activeCard : ''}`} key={card._id} onClick={() => handleSelectedCard(index)}>
-                      <h4>{card.title}</h4>
-                      {card.syntax && (
-                        <p>
-                          <span style={{ display: "inline-flex" }}><MessageIcon /> {card.syntax}</span>
-                        </p>
-                      )}
+              </div>
+            </Box>
+            <Box component="section" sx={{ p: 2, backgroundColor: '#a4d1d6' }}>
+              <div className={styles.introduceNumber}>
+                <Typography
+                  variant="h5"
+                  gutterBottom
+                  sx={{ color: theme === 'light' ? '#000' : '#fff' }}
+                >
+                  Giới thiệu
+                </Typography>
+                <Typography
+                  variant="subtitle1"
+                  gutterBottom
+                  sx={{
+                    color: theme === 'light' ? '#000' : '#fff',
+                    width: '65%',
+                    textAlign: 'center',
+                    lineHeight: 2
+                  }}
+                >
+                  Tổng đài 156 là đầu số miễn phí của Bộ Thông tin và Truyền thông, dùng để tiếp nhận phản ánh về cuộc gọi rác, tin nhắn rác và các cuộc gọi có dấu hiệu lừa đảo. Áp dụng toàn quốc, trên tất cả các nhà mạng di động và cố định.
+                </Typography>
+              </div>
+              <div className={styles.cards}>
+                {cards.map((card, index) => (
+                  <div className={`${styles.card} ${selectedCard == index ? styles.activeCard : ''}`} key={card._id} onClick={() => handleSelectedCard(index)}>
+                    <h4>{card.title}</h4>
+                    {card.syntax && (
+                      <p>
+                        <span style={{ display: "inline-flex" }}><MessageIcon /> {card.syntax}</span>
+                      </p>
+                    )}
 
-                      {card.phone && (
-                        <p>
-                          <span style={{ display: "inline-flex" }}><PhoneIcon /> {card.phone}</span>
-                        </p>
-                      )}
-                    </div>
-                  ))}
-                </div>
-                <div className={styles.buttonEmail}>
-                  <Button
-                    variant="contained"
-                    size="medium"
-                    color="success"
-                    onClick={() => setOpen(true)}>Xem mẫu email</Button>
-                </div>
-              </Box>
-            </div>
-          </main>
-          <Footer />
-        </div>
-        <ShareApps />
-      </section>
-      <Dialog
-        open={open}
-        onClose={() => setOpen(false)}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle>Hướng dẫn mẫu Email</DialogTitle>
-        <DialogContent dividers>
-          <DialogContentText id="alert-dialog-description">
-            <Typography gutterBottom>
-              Tiêu đề email:Phản ánh số điện thoại nghi lừa đảo [số điện thoại]
-            </Typography>
-
-            <Typography gutterBottom>
-              Kính gửi [Tên nhà mạng],
-              Tôi tên là [Họ tên], số thuê bao [số của bạn]. Tôi muốn yêu cầu hỗ trợ tra cứu nguồn gốc / chặn số điện thoại [số điện thoại] do [lý do: quấy rối / nghi lừa đảo / quảng cáo không mong muốn]. Xin hướng dẫn thủ tục và thời gian xử lý.
-              <p>Cảm ơn</p>
-            </Typography>
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={() => setOpen(false)} autoFocus>
-            Đóng
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </>
+                    {card.phone && (
+                      <p>
+                        <span style={{ display: "inline-flex" }}><PhoneIcon /> {card.phone}</span>
+                      </p>
+                    )}
+                  </div>
+                ))}
+              </div>
+            </Box>
+          </div>
+        </main>
+        <Footer />
+      </div>
+      <ShareApps />
+    </section>
   );
 }
