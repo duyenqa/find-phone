8b4682a41bcb698ef24f76ad8bfe243fa202a09b
duyenqa <ttemp8399@gmail.com>
2026-01-27 09:55:23 +0700

    update about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { useTheme } from "@/context/ThemeContext";
-import styles from "./about.module.css";
 import Navbar from "@/components/navbar/Navbar";
+import styles from "./about.module.css";
 
 export default function about() {
     const { theme } = useTheme();
@@ -11,10 +11,16 @@ export default function about() {
             <div className={styles.wrapper}>
                 <Navbar />
                 <div className={styles.headingBox}>
-                    <h1 className={`${styles.title} ${styles[theme]}`}>Trang về tôi</h1>
+                    <h1 className={`${styles.text} ${styles[theme]}`}>Trang về tôi</h1>
                 </div>
                 <div className={styles.boxAbout}>
-                    <p className={`${styles.title} ${styles[theme]}`}>Hello World, Duyên</p>
+                    <p className={`${styles.text} ${styles[theme]}`}>Ứng dụng Find Phone có giao diện thân thiện, dễ sử dụng, miễn phí cho cộng đồng người dùng trong Việt Nam.</p>
+                    <p className={`${styles.text} ${styles[theme]}`}>
+                        Phiên bản: 2.0 - 2025 - {new Date().getFullYear()}
+                    </p>
+                    <p className={`${styles.text} ${styles[theme]}`}>
+                        Tác giả bản quyền: Ngô Thị Kim Duyên
+                    </p>
                 </div>
             </div>
         </section>
